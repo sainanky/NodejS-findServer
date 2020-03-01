5f62bc2dfd768283d2c650e623e5572c37bf11df
ankit <ankit.kumar@pplfy.com>
2020-03-01 10:40:24 +0530

    add comments

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const Server = require('./findServer'); // fileServer.js contains all logical calculations
 const data  = require('./data.json'); // data.json contains list of servers
 const message = require('./message_template'); // message_template is an template of messages
+// comment
 
 Server.findServer(data).then((server)=>{
     console.log(`${message.online} ${server.url}`);
